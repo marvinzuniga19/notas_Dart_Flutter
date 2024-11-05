62525d290ef5b25e12bf578f1670fc35cfc5c274
“Marvin <marvin_zuniga19@outlook.com>
2024-11-05 10:33:10 -0600

    more notes

--- 21.future.dart
+++ 21.future.dart
@@ -1,15 +1,21 @@
+import 'dart:async';
+
 void main() {
   print('inicio del programa');
 
-  httpGet('https://marvin.com/portafolio').then((value){
+  httpGet('https://marvin.com/portafolio').then((value) {
     print(value);
+  }).catchError((err) {
+    print('Error: $err');
   });
-  
+
   print('fin del programa');
 }
 
 Future<String> httpGet(String url) {
-  return Future.delayed(Duration(seconds: 1), () {
-    return 'Respuesta de la peticion http';
+  return Future.delayed(Duration(seconds: 2), () {
+    throw 'Error en la peticion http';
+
+    // return 'Respuesta de la peticion http';
   });
 }
